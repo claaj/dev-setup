8c1dd9edbfe4dee2598d05c3d772c5d4af329a11
Matías Cajal <matias.cajal@hotmail.com>
2024-11-21 16:24:16 -0300

    Eliminar keybinds sin usos

--- nvim/.config/nvim/lua/custom/core/keymaps.lua
+++ nvim/.config/nvim/lua/custom/core/keymaps.lua
@@ -16,10 +16,6 @@ keymap.set("n", "<leader>sq", ":nohl<CR>", { desc = "Clear search highlights" })
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
 
--- increment/decrement numbers
-keymap.set("n", "<leader>ni", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>nd", "<C-x>", { desc = "Decrement number" }) -- decrement
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
