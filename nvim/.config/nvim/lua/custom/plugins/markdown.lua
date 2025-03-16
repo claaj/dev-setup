d08906d797cc20e0c56f711fd9d32d696a2ce85f
Matías Cajal <matias.cajal@hotmail.com>
2025-03-16 14:23:31 -0300

    Add markdown plugin

--- nvim/.config/nvim/lua/custom/plugins/markdown.lua
+++ nvim/.config/nvim/lua/custom/plugins/markdown.lua
@@ -3,4 +3,12 @@ return {
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 	},
+
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 }
