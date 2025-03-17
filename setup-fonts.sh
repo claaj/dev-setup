c3242889473067c6a97d24cfaec80f4b39f6082f
Matías Cajal <matias.cajal@hotmail.com>
2025-03-17 16:13:11 -0300

    Remove Iosevka. Use JetBrains Mono. Update download script

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-IOSEVKA_VERSION="33.1.0"
 NERD_FONTS_VERSION="v3.3.0"
 
-URL1="https://github.com/be5invis/Iosevka/releases/download/v${IOSEVKA_VERSION}/SuperTTC-Iosevka-${IOSEVKA_VERSION}.zip"
-URL2="https://github.com/ryanoasis/nerd-fonts/releases/download/$NERD_FONTS_VERSION/NerdFontsSymbolsOnly.zip"
+URL="https://github.com/ryanoasis/nerd-fonts/releases/download/$NERD_FONTS_VERSION/JetBrainsMono.zip"
 
 FONT_DIR="$HOME/.local/share/fonts"
 
 mkdir -p "$FONT_DIR"
 
-curl -L -o "/tmp/Iosevka.zip" "$URL1"
+curl -L -o "/tmp/JetBrainsMono.zip" "$URL"
 
-curl -L -o "/tmp/NerdFontsSymbolsOnly.zip" "$URL2"
+unzip -o "/tmp/JetBrainsMono.zip" -d "$FONT_DIR"
 
-unzip -o "/tmp/Iosevka.zip" -d "$FONT_DIR/Iosevka"
-unzip -o "/tmp/NerdFontsSymbolsOnly.zip" -d "$FONT_DIR/NerdFontsSymbolsOnly"
-
-rm "/tmp/Iosevka.zip" "/tmp/NerdFontsSymbolsOnly.zip"
+rm "/tmp/JetBrainsMono.zip"
 
 fc-cache -f "$FONT_DIR"
 
