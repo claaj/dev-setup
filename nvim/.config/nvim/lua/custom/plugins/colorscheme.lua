ce867d6933bdb3e285b5903e0e653ead21698deb
Matías Cajal <matias.cajal@hotmail.com>
2025-03-20 02:28:48 -0300

    Revert to Carbonfox

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,20 +1,8 @@
--- return {
---
--- 	"EdenEast/nightfox.nvim",
--- 	priority = 1000,
--- 	lazy = false,
--- 	config = function()
--- 		vim.cmd("colorscheme carbonfox")
--- 	end,
--- }
-
 return {
-	{
-		"oonamo/ef-themes.nvim",
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd("colorscheme ef-dark")
-		end,
-	},
+	"EdenEast/nightfox.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd("colorscheme carbonfox")
+	end,
 }
