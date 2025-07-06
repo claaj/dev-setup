5c7d317d3e4a2801565c28f45981a5b01eb04766
Maquinola <matias.cajal@hotmail.com>
2025-07-06 17:32:20 -0300

    Update devpod script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-brew install just
-brew install pipx
-brew install node
-brew install tmux
-brew install stow
-brew install fzf
-brew install eza
-brew install fd
-brew install ripgrep
-brew install typst
-brew install btop
-brew install fastfetch
-brew install tmux-sessionizer
-brew install starship
-brew install gitui
-brew install --cask font-adwaita font-adwaita-mono-nerd-font font-ibm-plex-sans font-ibm-plex-math font-twitter-color-emoji
+/home/linuxbrew/.linuxbrew/bin/brew install just
+/home/linuxbrew/.linuxbrew/bin/brew install pipx
+/home/linuxbrew/.linuxbrew/bin/brew install node
+/home/linuxbrew/.linuxbrew/bin/brew install tmux
+/home/linuxbrew/.linuxbrew/bin/brew install stow
+/home/linuxbrew/.linuxbrew/bin/brew install fzf
+/home/linuxbrew/.linuxbrew/bin/brew install eza
+/home/linuxbrew/.linuxbrew/bin/brew install fd
+/home/linuxbrew/.linuxbrew/bin/brew install ripgrep
+/home/linuxbrew/.linuxbrew/bin/brew install typst
+/home/linuxbrew/.linuxbrew/bin/brew install btop
+/home/linuxbrew/.linuxbrew/bin/brew install fastfetch
+/home/linuxbrew/.linuxbrew/bin/brew install tmux-sessionizer
+/home/linuxbrew/.linuxbrew/bin/brew install starship
+/home/linuxbrew/.linuxbrew/bin/brew install gitui
+/home/linuxbrew/.linuxbrew/bin/brew install --cask font-adwaita font-adwaita-mono-nerd-font font-ibm-plex-sans font-ibm-plex-math font-twitter-color-emoji
+
+touch ~/.bashrc
 
 echo 'eval "$$(starship init bash)"' >> "~/.bashrc"
 echo 'alias vim="nvim"' >> "~/.bashrc"
 echo 'alias ll="eza -la --icons --color=always"' >> "~/.bashrc"
 
-stow fontconfig tmux nvim zed
-
-fc-cache -fvr
+/home/linuxbrew/.linuxbrew/bin/stow fontconfig tmux nvim zed
