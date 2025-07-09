6418f483607dee1c793d82d8043adcd3d0d05aa5
Maquinola <matias.cajal@hotmail.com>
2025-07-09 14:01:23 -0300

    Add icons to nvim.tree

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -122,6 +122,9 @@ return {
   },
   {
     "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     keys = {
       { "<leader>ft", ":NvimTreeToggle<CR>", desc = "Toggle tree" },
     },
