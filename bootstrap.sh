b61833ab44aecedfafcdf50b7fadcd1167eace28
Maquinola <matias.cajal@hotmail.com>
2025-08-05 01:08:22 -0300

    Update devpod script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-# Descargar e instalar Neovim
-wget https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.tar.gz
-tar xzf nvim-linux-x86_64.tar.gz
+wget https://github.com/MordechaiHadad/bob/releases/download/v4.0.3/bob-linux-x86_64.zip
+unzip bob-linux-x86_64.zip
+chmod +x bob
 mkdir -p $HOME/.local/bin
-mv nvim-linux-x86_64 $HOME/.local/nvim
-ln -sf $HOME/.local/nvim/bin/nvim $HOME/.local/bin/nvim
-rm nvim-linux-x86_64.tar.gz
+mv bob $HOME/.local/bin/bob-nvim
+rm bob-linux-x86_64.zip
 
-echo 'alias vim="$HOME/.local/nvim/bin/nvim"' >> $HOME/.bashrc
+echo 'alias vim="nvim"' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/.local/share/bob/nvim-bin' >> "$HOME/.bashrc"
 
-echo "Neovim instalado correctamente"
+echo "bob-nvim instalado correctamente"
 
 stow nvim/ tmux/
+
+
