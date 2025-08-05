d707f7103fc3c2b0a7cea84cc40aa8227de8660a
Matías Cajal <102485147+claaj@users.noreply.github.com>
2025-08-05 08:55:59 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,13 @@
 wget https://github.com/MordechaiHadad/bob/releases/download/v4.0.3/bob-linux-x86_64.zip
 unzip bob-linux-x86_64.zip
 chmod +x bob
-mkdir -p $HOME/.local/bin
-mv bob $HOME/.local/bin/bob-nvim
+mkdir -p ~/.local/bin
+mv bob ~/.local/bin/bob
 rm bob-linux-x86_64.zip
 
-echo 'alias vim="nvim"' >> $HOME/.bashrc
-echo 'export PATH=$PATH:$HOME/.local/share/bob/nvim-bin' >> "$HOME/.bashrc"
+echo 'alias vim="nvim"' >> "~/.bashrc"
+echo 'export PATH=$PATH:$HOME/.local/share/bob/nvim-bin' >> "~/.bashrc"
 
 echo "bob-nvim instalado correctamente"
 
 stow nvim/ tmux/
-
-
