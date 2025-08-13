d5de12017986739aeb9e7b7903e20a53cde0275f
Maquinola <matias.cajal@hotmail.com>
2025-08-13 00:02:13 -0300

    Update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font("Adwaita Mono Nerd Font")
-config.color_scheme = 'Modus-Vivendi'
+config.color_scheme = 'Jellybeans'
 config.enable_tab_bar = false
 
 return config
